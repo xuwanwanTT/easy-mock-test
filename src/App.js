c07eac942dda5163911e7c3fe11a90beabc59eb2
xuwanwan <xuwanwan@outlook.com>
2017-11-29 01:12:16 +0800

    nothing

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ class App extends Component {
     axios.get('https://www.easy-mock.com/mock/5a1d898d8e6ddb24964d0a02/user/xww').then(res=>{
       console.log(res.data)
     });
+
     return (
       <div className="App">
         hello world
